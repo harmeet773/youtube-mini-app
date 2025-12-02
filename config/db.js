a1d0b99780f421ad87714df84baa09944338738f
Harmeet Singh Chugga <harmeetsinghchugga@gmail.com>
2025-12-02 15:55:55 +0530

    added ssl  certificate for production environment

--- config/db.js
+++ config/db.js
@@ -4,10 +4,21 @@ const { Sequelize } = require("sequelize");
 // Sequelize is an ORM  , it help to work with  SQL databases .
 //  We need to provide dialect so that Sequelize knows with with DB we are working with so it can load necesarry drivers  and interact with DB accordingly .
 
-if (process.env.Project_Host_Environment_Local === "true"){
-  
-}
-const sequelize = new Sequelize(
+let sequelize;
+if (process.env.Project_Host_Environment_Local === "true"){ sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASS,
+  {
+    host: process.env.DB_HOST,
+    dialect: process.env.DB_DIALECT,
+     port: process.env.DB_PORT,
+  }   
+);   
+
+
+}else{
+ sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
@@ -26,7 +37,7 @@ const sequelize = new Sequelize(
 
     logging: false,
   }
-); 
+);} 
 async function runSql(sql, params = []) {
   try {
     const [result] = await sequelize.query(sql, {
