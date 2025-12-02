aef56ef0a471004fa74a206bfb2744f81906bce3
Harmeet Singh Chugga <harmeetsinghchugga@gmail.com>
2025-12-02 13:14:42 +0530

    added TLS certificate in db.js

--- config/db.js
+++ config/db.js
@@ -1,17 +1,28 @@
 // db.js
 require("dotenv").config();
 const { Sequelize } = require("sequelize");
-
+// Sequelize is an ORM  , it help to work with  SQL databases .
+//  We need to provide dialect so that Sequelize knows with with DB we are working with so it can load necesarry drivers  and interact with DB accordingly .
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
   process.env.DB_PASS,
   {
     host: process.env.DB_HOST,
-    dialect: process.env.DB_DIALECT,
-  }
-);
+    port: process.env.DB_PORT,      // FIXED
+    dialect: process.env.DB_DIALECT, // should be "mysql"
 
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: true,
+        ca: process.env.DB_CA_CERT,   // full ca.pem content as env var
+      },
+    },
+
+    logging: false,
+  }
+); 
 async function runSql(sql, params = []) {
   try {
     const [result] = await sequelize.query(sql, {
